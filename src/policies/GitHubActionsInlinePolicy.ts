c42282fb8b7a36fe60a10f42f5a96704fad872a8
Jayson Rawlins <jrawlins@cloudlizzy.com>
2023-06-10 21:08:42 -0400

    refactor(githubactionsstack): updated iam roles to use cdk floyd
    re #1

--- src/policies/GitHubActionsInlinePolicy.ts
+++ src/policies/GitHubActionsInlinePolicy.ts
@@ -1,52 +1,45 @@
-import { Effect, PolicyDocument, PolicyStatement } from 'aws-cdk-lib/aws-iam';
+import { PolicyDocument } from 'aws-cdk-lib/aws-iam';
+import * as floyd from 'cdk-iam-floyd';
+
+if (floyd) {
+}
 
 export const GitHubActionsInlinePolicy = {
   PowerUserConstraints: new PolicyDocument({
     statements: [
-      new PolicyStatement({
-        sid: 'DefinitelyNotThese',
-        actions: [
-          'iam:PutGroupPolicy',
-          'iam:PutUserPolicy',
-          'iam:AddClientIDToOpenIDConnectProvider',
-          'iam:AddUserToGroup',
-          'iam:AttachUserPolicy',
-          'iam:CreateLoginProfile',
-          'iam:CreateSAMLProvider',
-          'iam:CreateUser',
-          'iam:CreateVirtualMFADevice',
-          'iam:DeleteAccountAlias',
-          'iam:DeleteAccountPasswordPolicy',
-          'iam:DeleteSAMLProvider',
-          'iam:RemoveClientIDFromOpenIDConnectProvider',
-          'iam:UpdateAccountPasswordPolicy',
-          'iam:UpdateOpenIDConnectProviderThumbprint',
-          'iam:UpdateSAMLProvider',
-        ],
-        resources: ['*'],
-        effect: Effect.DENY,
-      }),
-      new PolicyStatement({
-        sid: 'AllowThisEveryWhere',
+      new floyd.Iam().deny().onAllResources()
+        .toPutGroupPolicy()
+        .toPutUserPolicy()
+        .toAddClientIDToOpenIDConnectProvider()
+        .toAddUserToGroup()
+        .toAttachUserPolicy()
+        .toCreateLoginProfile()
+        .toCreateSAMLProvider()
+        .toCreateUser()
+        .toCreateVirtualMFADevice()
+        .toDeleteAccountAlias()
+        .toDeleteAccountPasswordPolicy()
+        .toDeleteSAMLProvider()
+        .toRemoveClientIDFromOpenIDConnectProvider()
+        .toUpdateAccountPasswordPolicy()
+        .toUpdateOpenIDConnectProviderThumbprint()
+        .toUpdateSAMLProvider()
+      ,
+      new floyd.Iam({
         actions: [
           'iam:Get*',
           'iam:List*',
-          'iam:GenerateCredentialReport',
-          'iam:SimulateCustomPolicy',
-          'iam:SimulatePrincipalPolicy',
         ],
-        resources: ['*'],
-        effect: Effect.ALLOW,
-      }),
-      new PolicyStatement({
-        sid: 'DenyOrgAccountsAccess',
-        effect: Effect.DENY,
-        resources: ['*'],
+      }).allow().onAllResources().toGenerateCredentialReport()
+        .toSimulateCustomPolicy()
+        .toSimulatePrincipalPolicy()
+      ,
+      new floyd.Organizations({
         actions: [
           'organizations:*',
           'account:*',
         ],
-      }),
+      }).deny().onAllResources(),
     ],
   }),
 };
